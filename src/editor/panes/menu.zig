91cd768523d95677c863208c94e0483d1009e37f
foxnne <foxnne@gmail.com>
2022-10-20 17:58:06 -0500

    zig-gamedev: Update to latest

--- src/editor/panes/menu.zig
+++ src/editor/panes/menu.zig
@@ -6,6 +6,8 @@ const filebrowser = @import("filebrowser");
 const nfd = @import("nfd");
 
 pub fn draw() void {
+    zgui.pushStyleVar2f(.{ .idx = zgui.StyleVar.window_padding, .v = .{ 10.0 * pixi.state.window.scale[0], 10.0 * pixi.state.window.scale[1] }});
+    defer zgui.popStyleVar(.{ .count = 1 });
     zgui.pushStyleColor4f(.{ .idx = zgui.StyleCol.text, .c = pixi.state.style.text_secondary.toSlice() });
     zgui.pushStyleColor4f(.{ .idx = zgui.StyleCol.popup_bg, .c = pixi.state.style.foreground.toSlice() });
     defer zgui.popStyleColor(.{ .count = 2 });
