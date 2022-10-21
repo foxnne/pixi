0b491197962bdc1a03bae9fa36df90eeb0b26c5e
foxnne <foxnne@gmail.com>
2022-10-21 13:13:53 -0500

    build.zig: Copy `ensure` fns from zig-gamedev.

--- build.zig
+++ build.zig
@@ -27,6 +27,24 @@ const ProcessAssetsStep = @import("src/tools/process_assets.zig").ProcessAssetsS
 pub fn build(b: *Builder) !void {
     const target = b.standardTargetOptions(.{});
 
+    ensureTarget(target) catch return;
+    ensureGit(b.allocator) catch return;
+    ensureGitLfs(b.allocator, "install") catch return;
+    ensureGitLfs(b.allocator, "pull") catch return;
+
+    // Fetch the latest Dawn/WebGPU binaries.
+    {
+        var child = std.ChildProcess.init(&.{ "git", "submodule", "update", "--init", "--remote" }, b.allocator);
+        child.cwd = thisDir();
+        child.stderr = std.io.getStdErr();
+        child.stdout = std.io.getStdOut();
+        _ = child.spawnAndWait() catch {
+            std.log.err("Failed to fetch git submodule. Please try to re-clone.", .{});
+            return;
+        };
+    }
+    ensureGitLfsContent("/src/deps/zig-gamedev/zgpu/libs/dawn/x86_64-windows-gnu/dawn.lib") catch return;
+
     var exe = createExe(b, target, "run", "src/pixi.zig");
     b.default_step.dependOn(&exe.step);
 
@@ -68,9 +86,7 @@ fn createExe(b: *Builder, target: std.zig.CrossTarget, name: []const u8, source:
     if (b.is_release) {
         if (target.isWindows()) {
             exe.subsystem = .Windows;
-        }
-
-        if (builtin.os.tag == .macos and builtin.cpu.arch == std.Target.Cpu.Arch.aarch64) {
+        } else {
             exe.subsystem = .Posix;
         }
     }
@@ -105,4 +121,139 @@ fn createExe(b: *Builder, target: std.zig.CrossTarget, name: []const u8, source:
 
 inline fn thisDir() []const u8 {
     return comptime std.fs.path.dirname(@src().file) orelse ".";
+}
+
+fn ensureTarget(cross: std.zig.CrossTarget) !void {
+    const target = (std.zig.system.NativeTargetInfo.detect(cross) catch unreachable).target;
+
+    const supported = switch (target.os.tag) {
+        .windows => target.cpu.arch.isX86() and target.abi.isGnu(),
+        .linux => (target.cpu.arch.isX86() or target.cpu.arch.isAARCH64()) and target.abi.isGnu(),
+        .macos => blk: {
+            if (!target.cpu.arch.isX86() and !target.cpu.arch.isAARCH64()) break :blk false;
+
+            // If min. target macOS version is lesser than the min version we have available, then
+            // our Dawn binary is incompatible with the target.
+            const min_available = std.builtin.Version{ .major = 12, .minor = 0 };
+            if (target.os.version_range.semver.min.order(min_available) == .lt) break :blk false;
+            break :blk true;
+        },
+        else => false,
+    };
+    if (!supported) {
+        std.log.err("\n" ++
+            \\---------------------------------------------------------------------------
+            \\
+            \\Unsupported build target. Dawn/WebGPU binary for this target is not available.
+            \\
+            \\Following targets are supported:
+            \\
+            \\x86_64-windows-gnu
+            \\x86_64-linux-gnu
+            \\x86_64-macos.12-none
+            \\aarch64-linux-gnu
+            \\aarch64-macos.12-none
+            \\
+            \\---------------------------------------------------------------------------
+            \\
+        , .{});
+        return error.TargetNotSupported;
+    }
+}
+
+fn ensureGit(allocator: std.mem.Allocator) !void {
+    const printErrorMsg = (struct {
+        fn impl() void {
+            std.log.err("\n" ++
+                \\---------------------------------------------------------------------------
+                \\
+                \\'git version' failed. Is Git not installed?
+                \\
+                \\---------------------------------------------------------------------------
+                \\
+            , .{});
+        }
+    }).impl;
+    const argv = &[_][]const u8{ "git", "version" };
+    const result = std.ChildProcess.exec(.{
+        .allocator = allocator,
+        .argv = argv,
+        .cwd = ".",
+    }) catch { // e.g. FileNotFound
+        printErrorMsg();
+        return error.GitNotFound;
+    };
+    defer {
+        allocator.free(result.stderr);
+        allocator.free(result.stdout);
+    }
+    if (result.term.Exited != 0) {
+        printErrorMsg();
+        return error.GitNotFound;
+    }
+}
+
+fn ensureGitLfs(allocator: std.mem.Allocator, cmd: []const u8) !void {
+    const printNoGitLfs = (struct {
+        fn impl() void {
+            std.log.err("\n" ++
+                \\---------------------------------------------------------------------------
+                \\
+                \\Please install Git LFS (Large File Support) extension and run 'zig build' again.
+                \\
+                \\For more info about Git LFS see: https://git-lfs.github.com/
+                \\
+                \\---------------------------------------------------------------------------
+                \\
+            , .{});
+        }
+    }).impl;
+    const argv = &[_][]const u8{ "git", "lfs", cmd };
+    const result = std.ChildProcess.exec(.{
+        .allocator = allocator,
+        .argv = argv,
+        .cwd = ".",
+    }) catch { // e.g. FileNotFound
+        printNoGitLfs();
+        return error.GitLfsNotFound;
+    };
+    defer {
+        allocator.free(result.stderr);
+        allocator.free(result.stdout);
+    }
+    if (result.term.Exited != 0) {
+        printNoGitLfs();
+        return error.GitLfsNotFound;
+    }
+}
+
+fn ensureGitLfsContent(comptime file_path: []const u8) !void {
+    const printNoGitLfsContent = (struct {
+        fn impl() void {
+            std.log.err("\n" ++
+                \\---------------------------------------------------------------------------
+                \\
+                \\Something went wrong, Git LFS content has not been downloaded.
+                \\
+                \\Please try to re-clone the repo and build again.
+                \\
+                \\---------------------------------------------------------------------------
+                \\
+            , .{});
+        }
+    }).impl;
+    const file = std.fs.openFileAbsolute(thisDir() ++ file_path, .{}) catch {
+        printNoGitLfsContent();
+        return error.GitLfsNoContent;
+    };
+    defer file.close();
+
+    const size = file.getEndPos() catch {
+        printNoGitLfsContent();
+        return error.GitLfsNoContent;
+    };
+    if (size <= 1024) {
+        printNoGitLfsContent();
+        return error.GitLfsNoContent;
+    }
 }
