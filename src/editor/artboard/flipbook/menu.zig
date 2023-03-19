b9fc88bce47ed48156b7042df1cded2783f0f8e6
foxnne <foxnne@gmail.com>
2023-03-19 16:42:32 -0500

    artboard: Add animation combo box to menu bar

--- src/editor/artboard/flipbook/menu.zig
+++ src/editor/artboard/flipbook/menu.zig
@@ -20,7 +20,24 @@ pub fn draw(file: *pixi.storage.Internal.Pixi, mouse_ratio: f32) void {
                 .pause => .play,
             };
         }
-        if (zgui.combo("Animation", .{})) {}
+        if (file.animations.items.len > 0) {
+            zgui.pushStyleColor4f(.{ .idx = zgui.StyleCol.frame_bg_hovered, .c = pixi.state.style.background.toSlice() });
+            zgui.pushStyleColor4f(.{ .idx = zgui.StyleCol.frame_bg, .c = pixi.state.style.background.toSlice() });
+            defer zgui.popStyleColor(.{ .count = 2 });
+            const animation_name = file.animations.items[file.selected_animation_index].name;
+            zgui.setNextItemWidth(zgui.calcTextSize(animation_name, .{})[0] + 40 * pixi.state.window.scale[0]);
+
+            if (zgui.beginCombo("Animation", .{ .preview_value = animation_name, .flags = .{ .height_small = true } })) {
+                defer zgui.endCombo();
+                for (file.animations.items, 0..) |animation, index| {
+                    if (zgui.selectable(animation.name, .{ .selected = index == file.selected_animation_index })) {
+                        file.selected_animation_index = index;
+                        file.flipbook_scroll_request = .{ .from = file.flipbook_scroll, .to = file.flipbookScrollFromSpriteIndex(animation.start), .state = file.selected_animation_state };
+                    }
+                }
+            }
+        }
+
         _ = zgui.invisibleButton("FlipbookGrip", .{
             .w = -1.0,
             .h = -1.0,
