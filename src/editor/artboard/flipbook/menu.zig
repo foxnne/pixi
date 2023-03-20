623ef98e69e85f92ca3f4cd92bc30b8981383e53
foxnne <foxnne@gmail.com>
2023-03-19 20:51:00 -0500

    flipbook: Added frame counter and selection combo to menu

--- src/editor/artboard/flipbook/menu.zig
+++ src/editor/artboard/flipbook/menu.zig
@@ -13,26 +13,37 @@ pub fn draw(file: *pixi.storage.Internal.Pixi, mouse_ratio: f32) void {
     defer zgui.popStyleColor(.{ .count = 3 });
     if (zgui.beginMenuBar()) {
         defer zgui.endMenuBar();
-
-        if (zgui.button(if (file.selected_animation_state == .play) " " ++ pixi.fa.pause ++ " " else " " ++ pixi.fa.play ++ " ", .{})) {
-            file.selected_animation_state = switch (file.selected_animation_state) {
-                .play => .pause,
-                .pause => .play,
-            };
-        }
         if (file.animations.items.len > 0) {
+            if (zgui.button(if (file.selected_animation_state == .play) " " ++ pixi.fa.pause ++ " " else " " ++ pixi.fa.play ++ " ", .{})) {
+                file.selected_animation_state = switch (file.selected_animation_state) {
+                    .play => .pause,
+                    .pause => .play,
+                };
+            }
+
             zgui.pushStyleColor4f(.{ .idx = zgui.StyleCol.frame_bg_hovered, .c = pixi.state.style.background.toSlice() });
             zgui.pushStyleColor4f(.{ .idx = zgui.StyleCol.frame_bg, .c = pixi.state.style.background.toSlice() });
             defer zgui.popStyleColor(.{ .count = 2 });
-            const animation_name = file.animations.items[file.selected_animation_index].name;
-            zgui.setNextItemWidth(zgui.calcTextSize(animation_name, .{})[0] + 40 * pixi.state.window.scale[0]);
+            const animation = file.animations.items[file.selected_animation_index];
+            zgui.setNextItemWidth(zgui.calcTextSize(animation.name, .{})[0] + 40 * pixi.state.window.scale[0]);
 
-            if (zgui.beginCombo("Animation", .{ .preview_value = animation_name, .flags = .{ .height_small = true } })) {
+            if (zgui.beginCombo("Animation  ", .{ .preview_value = animation.name, .flags = .{ .height_largest = true } })) {
                 defer zgui.endCombo();
-                for (file.animations.items, 0..) |animation, index| {
-                    if (zgui.selectable(animation.name, .{ .selected = index == file.selected_animation_index })) {
-                        file.selected_animation_index = index;
-                        file.flipbook_scroll_request = .{ .from = file.flipbook_scroll, .to = file.flipbookScrollFromSpriteIndex(animation.start), .state = file.selected_animation_state };
+                for (file.animations.items, 0..) |a, i| {
+                    if (zgui.selectable(a.name, .{ .selected = i == file.selected_animation_index })) {
+                        file.selected_animation_index = i;
+                        file.flipbook_scroll_request = .{ .from = file.flipbook_scroll, .to = file.flipbookScrollFromSpriteIndex(a.start), .state = file.selected_animation_state };
+                    }
+                }
+            }
+            const current_frame = if (file.selected_sprite_index > animation.start) file.selected_sprite_index - animation.start else 0;
+            const frame = zgui.formatZ("{d}/{d}", .{ current_frame + 1, animation.length });
+            zgui.setNextItemWidth(zgui.calcTextSize(frame, .{})[0] + 40 * pixi.state.window.scale[0]);
+            if (zgui.beginCombo("Frame  ", .{ .preview_value = frame })) {
+                defer zgui.endCombo();
+                for (0..animation.length) |i| {
+                    if (zgui.selectable(zgui.formatZ("{d}/{d}", .{ i + 1, animation.length }), .{ .selected = animation.start + i == file.selected_animation_index })) {
+                        file.flipbook_scroll_request = .{ .from = file.flipbook_scroll, .to = file.flipbookScrollFromSpriteIndex(animation.start + i), .state = file.selected_animation_state };
                     }
                 }
             }
