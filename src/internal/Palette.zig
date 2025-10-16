e3a5ae9fba6c8639d5860b71a72a664c5c0d3ef4
foxnne <foxnne@gmail.com>
2025-10-15 20:29:42 -0500

    Palette: Fix broken palette loading

--- src/internal/Palette.zig
+++ src/internal/Palette.zig
@@ -20,22 +20,37 @@ pub fn loadFromFile(file: []const u8) !Palette {
     const ext = std.fs.path.extension(file);
 
     if (std.mem.eql(u8, ext, ".hex")) {
-        var contents = try std.fs.cwd().openFile(file, .{});
-        defer contents.close();
+        if (pixi.fs.read(pixi.app.allocator, file) catch null) |read| {
+            defer pixi.app.allocator.free(read);
 
-        var buf: [1000]u8 = undefined;
-        var reader = contents.reader(&buf);
-        var interface = &reader.interface;
-
-        while (interface.takeDelimiterExclusive('\n') catch null) |line| {
-            const color_u32 = std.fmt.parseInt(u32, line[0 .. line.len - 1], 16) catch {
-                dvui.log.err("Failed to parse color: {s}", .{line[0 .. line.len - 1]});
-                return error.FailedToParseColor;
-            };
-            const color_packed: PackedColor = @as(PackedColor, @bitCast(color_u32));
-            try colors.append(.{ color_packed.b, color_packed.g, color_packed.r, 255 });
-            //pixi.app.allocator.free(line);
+            var iter = std.mem.splitSequence(u8, read, "\n");
+            while (iter.next()) |line| {
+                if (line.len == 0) continue;
+                const color_u32 = std.fmt.parseInt(u32, line[0 .. line.len - 1], 16) catch {
+                    dvui.log.err("Failed to parse color: {s}", .{line[0 .. line.len - 1]});
+                    return error.FailedToParseColor;
+                };
+                const color_packed: PackedColor = @as(PackedColor, @bitCast(color_u32));
+                try colors.append(.{ color_packed.b, color_packed.g, color_packed.r, 255 });
+            }
         }
+        // var contents = try std.fs.cwd().openFile(file, .{});
+        // defer contents.close();
+
+        // var buf: [1000]u8 = undefined;
+        // var reader = contents.reader(&buf);
+        // var interface = &reader.interface;
+
+        // while (interface.takeDelimiterExclusive('\n') catch null) |line| {
+        //     //if (line.len == 0) continue;
+        //     const color_u32 = std.fmt.parseInt(u32, line[0 .. line.len - 1], 16) catch {
+        //         dvui.log.err("Failed to parse color: {s}", .{line[0 .. line.len - 1]});
+        //         return error.FailedToParseColor;
+        //     };
+        //     const color_packed: PackedColor = @as(PackedColor, @bitCast(color_u32));
+        //     try colors.append(.{ color_packed.b, color_packed.g, color_packed.r, 255 });
+        //     //pixi.app.allocator.free(line);
+        // }
     } else {
         return error.WrongFileType;
     }
